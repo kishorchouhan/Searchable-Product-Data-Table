1ca97d47c05842f7416e3873e4a39b5002f115bf
kishorchouhan <devkishorbwr@gmail.com>
2019-06-09 23:06:48 +0530

    Updates

---
+++ precache-manifest.cad319ce49b782329ec6d411080bdd7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e34de881ff613757f5b585e01c66f69",
+    "url": "/Searchable-Product-Data-Table/index.html"
+  },
+  {
+    "revision": "72e58d66e3d8b654dbfc",
+    "url": "/Searchable-Product-Data-Table/static/css/main.274fd112.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/Searchable-Product-Data-Table/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "72e58d66e3d8b654dbfc",
+    "url": "/Searchable-Product-Data-Table/static/js/main.761c5ec2.chunk.js"
+  },
+  {
+    "revision": "4355000002bc831995ca",
+    "url": "/Searchable-Product-Data-Table/static/js/runtime~main.3bffea69.js"
+  }
+]);
